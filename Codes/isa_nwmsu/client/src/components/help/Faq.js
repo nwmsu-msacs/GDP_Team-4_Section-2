6161ce90abd1cc0ece129d5f99cfc4589863804c
bharat-reddy-male <bharatreddy1994@gmail.com>
2020-07-13 19:50:31 -0500

    basic UI for Faq functionality need to add CSS

--- Codes/isa_nwmsu/client/src/components/help/Faq.js
+++ Codes/isa_nwmsu/client/src/components/help/Faq.js
@@ -15,7 +15,83 @@ class Faq extends Component {
         return (
             <div>
 
-                
+{/* <!--Accordion wrapper--> */}
+<div class="accordion md-accordion" id="accordionEx1" role="tablist" aria-multiselectable="true">
+
+  {/* <!-- Accordion card --> */}
+  <div class="card">
+
+    {/* <!-- Card header --> */}
+    <div class="card-header" role="tab" id="headingTwo1">
+      <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx1" href="#collapseTwo1"
+        aria-expanded="false" aria-controls="collapseTwo1">
+        <h5 class="mb-0">
+          FAQ - 1 <i class="fas fa-angle-down rotate-icon"></i>
+        </h5>
+      </a>
+    </div>
+
+    {/* <!-- Card body --> */}
+    <div id="collapseTwo1" class="collapse" role="tabpanel" aria-labelledby="headingTwo1"
+      data-parent="#accordionEx1">
+      <div class="card-body">
+        FAQ Answer
+      </div>
+    </div>
+
+  </div>
+  {/* <!-- Accordion card --> */}
+
+  {/* <!-- Accordion card --> */}
+  <div class="card">
+
+    {/* <!-- Card header --> */}
+    <div class="card-header" role="tab" id="headingTwo2">
+      <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx1" href="#collapseTwo21"
+        aria-expanded="false" aria-controls="collapseTwo21">
+        <h5 class="mb-0">
+          FAQ - 2 <i class="fas fa-angle-down rotate-icon"></i>
+        </h5>
+      </a>
+    </div>
+
+    {/* <!-- Card body --> */}
+    <div id="collapseTwo21" class="collapse" role="tabpanel" aria-labelledby="headingTwo21"
+      data-parent="#accordionEx1">
+      <div class="card-body">
+        FAQ Answer
+      </div>
+    </div>
+
+  </div>
+  {/* <!-- Accordion card --> */}
+
+  {/* <!-- Accordion card --> */}
+  <div class="card">
+
+    {/* <!-- Card header --> */}
+    <div class="card-header" role="tab" id="headingThree31">
+      <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx1" href="#collapseThree31"
+        aria-expanded="false" aria-controls="collapseThree31">
+        <h5 class="mb-0">
+          FAQ - 3<i class="fas fa-angle-down rotate-icon"></i>
+        </h5>
+      </a>
+    </div>
+
+    {/* <!-- Card body --> */}
+    <div id="collapseThree31" class="collapse" role="tabpanel" aria-labelledby="headingThree31"
+      data-parent="#accordionEx1">
+      <div class="card-body">
+        FAQ Answer
+      </div>
+    </div>
+
+  </div>
+  {/* <!-- Accordion card --> */}
+
+</div>
+{/* <!-- Accordion wrapper --> */}
             </div>
 
 
