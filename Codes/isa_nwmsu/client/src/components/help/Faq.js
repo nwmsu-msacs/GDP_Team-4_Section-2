428e0ce784763c1bfd67db5b45a640afe05c52b6
bharat-reddy-male <bharatreddy1994@gmail.com>
2020-07-15 02:58:30 -0500

    FAQ page updated with div container

--- Codes/isa_nwmsu/client/src/components/help/Faq.js
+++ Codes/isa_nwmsu/client/src/components/help/Faq.js
@@ -13,7 +13,9 @@ class Faq extends Component {
         const { errors } = this.state;
 
         return (
-            <div>
+            <div><br/>
+            
+            <div class="container">
 
 {/* <!--Accordion wrapper--> */}
 <div class="accordion md-accordion" id="accordionEx1" role="tablist" aria-multiselectable="true">
@@ -23,7 +25,7 @@ class Faq extends Component {
 
     {/* <!-- Card header --> */}
     <div class="card-header" role="tab" id="headingTwo1">
-      <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx1" href="#collapseTwo1"
+      <a class="collapsed"  style={{textDecoration:"none"}} data-toggle="collapse" data-parent="#accordionEx1" href="#collapseTwo1"
         aria-expanded="false" aria-controls="collapseTwo1">
         <h5 class="mb-0">
           FAQ - 1 <i class="fas fa-angle-down rotate-icon"></i>
@@ -47,7 +49,7 @@ class Faq extends Component {
 
     {/* <!-- Card header --> */}
     <div class="card-header" role="tab" id="headingTwo2">
-      <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx1" href="#collapseTwo21"
+      <a class="collapsed" style={{textDecoration:"none"}} data-toggle="collapse" data-parent="#accordionEx1" href="#collapseTwo21"
         aria-expanded="false" aria-controls="collapseTwo21">
         <h5 class="mb-0">
           FAQ - 2 <i class="fas fa-angle-down rotate-icon"></i>
@@ -71,10 +73,10 @@ class Faq extends Component {
 
     {/* <!-- Card header --> */}
     <div class="card-header" role="tab" id="headingThree31">
-      <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx1" href="#collapseThree31"
+      <a class="collapsed" style={{textDecoration:"none"}}  data-toggle="collapse" data-parent="#accordionEx1" href="#collapseThree31"
         aria-expanded="false" aria-controls="collapseThree31">
         <h5 class="mb-0">
-          FAQ - 3<i class="fas fa-angle-down rotate-icon"></i>
+          FAQ - 3 <i class="fas fa-angle-down rotate-icon" ></i>
         </h5>
       </a>
     </div>
@@ -94,7 +96,7 @@ class Faq extends Component {
 {/* <!-- Accordion wrapper --> */}
             </div>
 
-
+            </div>
 
             
         );
