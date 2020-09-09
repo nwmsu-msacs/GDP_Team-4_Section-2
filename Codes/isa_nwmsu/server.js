c1d9d0788423932988719e8fb4380387ffcb77a1
Bhavya-123 <S536778@nwmissouri.edu>
2020-09-09 18:23:01 -0500

    added separate api file for events

--- Codes/isa_nwmsu/server.js
+++ Codes/isa_nwmsu/server.js
@@ -37,6 +37,6 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
-
+app.use("/api/events", require('./routes/api/events'));
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
