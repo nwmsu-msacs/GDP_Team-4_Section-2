d5935233eb5c9b9711e6880e5fc6c656642d6bd3
bharat-reddy-male <bharatreddy1994@gmail.com>
2020-09-14 03:12:32 -0500

    front end for accommodation

---
+++ Codes/isa_nwmsu/client/src/components/services/accommodation.js
@@ -0,0 +1,158 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import classnames from "classnames";
+import image from "../layout/assets/carousel_image4.jpg"
+import axios from 'axios';
+import Navbar from "../layout/Navbar";
+
+
+class Accommodation extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            firstName: "",
+            lastName: "",
+            non: "",
+            daysRequired: "",
+            gender: "",
+            contactNo: "",
+            errors: {}
+        };
+    }
+
+    onChange = e => {
+        this.setState({ [e.target.id]: e.target.value });
+    };
+
+    // onRadioChange = (e) => {
+    //         this.setState({
+    //           gender: e.target.value
+    //         });
+    //       }
+
+
+    onSubmit = e => {
+        e.preventDefault();
+
+        const accommodationData = {
+
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            non: this.state.non,
+            daysRequired: this.state.daysRequired,
+            gender: this.state.gender,
+            contactNo: this.state.contactNo,
+
+        };
+
+        
+
+        axios.post('http://localhost:5000/api/services/accommodation', accommodationData)
+            .then(res => console.log(res.data))
+            .then(this.props.history.push("/home"))
+    };
+
+    render() {
+        const { errors } = this.state;
+
+        return (
+            <div>
+                <Navbar />
+
+                <div class="container">
+                    <h3>Temporary Accommodation</h3>
+                    <form onSubmit={this.onSubmit}>
+                        {/* first name */}
+                        <label htmlFor="Name">First Name</label>
+                        <input
+                            type="text"
+                            name="firstName"
+                            id="firstName"
+                            value={this.state.firstName}
+                            onChange={this.onChange}
+                            placeholder="Enter first name"
+                        />
+
+                        {/* last name */}
+                        <label htmlFor="Name">last Name</label>
+                        <input
+                            type="text"
+                            name="lastName"
+                            id="lastName"
+                            value={this.state.lastName}
+                            onChange={this.onChange}
+                            placeholder="Enter last name"
+                        />
+                        {/* email */}
+                        <label htmlFor="non">919#</label>
+                        <input
+                            type="number"
+                            name="non"
+                            id="non"
+                            value={this.state.non}
+                            onChange={this.onChange}
+                            placeholder="Enter 919#"
+                        />
+
+                        {/* Gender */}
+
+
+                        <label>Gender&nbsp;&nbsp;</label><br />
+                        <label htmlFor="male">
+                            <input class="with-gap" onChange={this.onChange}
+                                value="Male"
+                                name="gender"
+                                type="radio" checked={this.state.gender === "Male"}
+                                checked={true}
+                            />
+                            <span>Male</span>
+                        </label>
+                                &nbsp;&nbsp;
+                                <label>
+                            <input class="with-gap" onChange={this.onChange}
+                                value="Female"
+                                name="gender"
+                                type="radio" checked={this.state.gender === "Female"}
+                            />
+                            <span>Female</span>
+                        </label>
+
+                        <br />
+                        {/* major */}
+                        <label htmlFor="daysRequired">Days Required</label>
+                        <input
+                            type="number"
+                            name="daysRequired"
+                            id="daysRequired"
+                            value={this.state.daysRequired}
+                            onChange={this.onChange}
+                            placeholder="Number of days"
+                        />
+                        {/* contact no */}
+                        <label htmlFor="contactNo">Contact Number</label>
+                        <input
+                            type="number"
+                            name="contactNo"
+                            id="contactNo"
+                            value={this.state.contactNo}
+                            onChange={this.onChange}
+                            placeholder="Enter Cell number"
+                        />
+
+
+                        {/* submit  */}
+                        <button
+                            type="submit"
+                            value="Request Pickup">Request Accommodation</button>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+
+
+}
+
+export default Accommodation;
