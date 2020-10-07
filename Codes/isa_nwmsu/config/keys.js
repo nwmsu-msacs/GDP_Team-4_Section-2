13fa7e11f93e1308bcb3c25eafc9b9f0761bc597
bharat-reddy-male <bharatreddy1994@gmail.com>
2020-10-06 19:47:55 -0500

    chat feature code

--- Codes/isa_nwmsu/config/keys.js
+++ Codes/isa_nwmsu/config/keys.js
@@ -1,4 +1,7 @@
 module.exports = {
     mongoURI: "mongodb+srv://isaadmin:isa@nwmissouri@isa-nwmsu-nppp3.mongodb.net/ISA-NWMSU?retryWrites=true&w=majority",
-    secretOrKey: "secret"
+    secretOrKey: "secret",
+    appID :"240050f8a85710c",
+    apiKey: "b1fd926620efd2b1bdd680838075dbdecc4402a5",
+    agentUID: "admin"
   };
