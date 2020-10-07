f3c3dfa5558a4267fe6cab17e3631d9863e9f574
bharat-reddy-male <bharatreddy1994@gmail.com>
2020-10-06 19:49:18 -0500

    chat code

--- Codes/isa_nwmsu/server.js
+++ Codes/isa_nwmsu/server.js
@@ -9,7 +9,8 @@ const passport = require("passport");
 const users = require("./routes/api/users");
 const events = require('./routes/api/events');
 const services = require('./routes/api/services');
-
+const chat = require('./routes/api/chat');
+const admin = require('./routes/api/admin')
 const app = express();
 
 // use Helmet middleware to automatically set secure HTTP headers
@@ -32,6 +33,23 @@ mongoose
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
 
+  //cometChat 
+
+// const appID = require("./config/keys").appID;
+// const apiKey = require("./config/keys").apiKey;
+// const agentUID = require("./config/keys").agentUID;
+
+const appID = '240050f8a85710c';
+const apiKey = '18d853100a77e50aed9fb3d75b4940ef552a4f18';
+const agentUID = 'admin'
+
+
+
+const headers = {
+  'Content-Type': 'application/json',
+  appid: appID,
+  apikey: apiKey,
+};
 
 // Passport middleware
 app.use(passport.initialize());
@@ -41,6 +59,8 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/events", events);
 app.use("/api/services", services);
+app.use("/api/chat", chat);
+app.use("/api/admin", admin);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
