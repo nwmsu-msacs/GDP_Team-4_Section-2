3ac0c7c81616ba1494d0e0bf805c5f2ed85e43f0
bharat-reddy-male <bharatreddy1994@gmail.com>
2020-10-09 21:22:56 -0500

    Navbar differentiated between admin and user

--- Codes/isa_nwmsu/client/src/components/home/home.js
+++ Codes/isa_nwmsu/client/src/components/home/home.js
@@ -3,62 +3,67 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
 import Navbar from "../layout/Navbar";
+import AdminNavbar from "../layout/AdminNavbar";
 import { Redirect } from "react-router-dom";
 // import Navbar from "../layout/Navbar";
 
+
+let navbar = undefined;
 class Home extends Component {
+
+  componentWillMount() {
+    if (localStorage.getItem('jwtToken') != null) {
+      this.setState({ loggedIn: true });
+    }
+    if (localStorage.getItem('role') === null || localStorage.getItem('role') === '50') {
+      navbar = <Navbar />
+    }
+    if (localStorage.getItem('role') === '100') {
+      navbar = <AdminNavbar />
+    }
+  }
+
   onLogoutClick = e => {
     e.preventDefault();
     this.props.logoutUser();
     // this.setState({loggedIn: false}); 
-    
+
   };
 
-  // componentDidMount(){
-  //   {Navbar}
-  // }
-
-
-  getNavBar=()=>{
-    return(
-    <Navbar/>
-    );
-  }
-render() {
-  console.log(this.props.auth);
+  render() {
+    console.log(this.props.auth);
     const { user } = this.props.auth;
 
+    return (
+      <div>
+        {navbar}
+        <div style={{ height: "75vh" }} className="container valign-wrapper">
 
-return (
-  <div>
-     {/* {this.getNavBar()}  */}
-      <div style={{ height: "75vh" }} className="container valign-wrapper">
-        
-        <div className="row">
-          <div className="col s12 center-align">
-            <h4>
-            <p className="flow-text grey-text text-darken-1">
-                Login Successful{" "}
-                
-              </p>
-            <b>Indian Student Association</b> welcomes, {user.name.split(" ")[0]}
-              
-            </h4>
-            <button
-              style={{
-                width: "150px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px",
-                marginTop: "1rem"
-              }}
-              onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-            >
-              Logout
+          <div className="row">
+            <div className="col s12 center-align">
+              <h4>
+                <p className="flow-text grey-text text-darken-1">
+                  Login Successful{" "}
+
+                </p>
+                <b>Indian Student Association</b> welcomes, {user.name.split(" ")[0]}
+
+              </h4>
+              <button
+                style={{
+                  width: "150px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px",
+                  marginTop: "1rem"
+                }}
+                onClick={this.onLogoutClick}
+                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+              >
+                Logout
             </button>
+            </div>
           </div>
         </div>
-      </div>
       </div>
     );
   }
