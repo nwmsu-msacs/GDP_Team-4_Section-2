5470a63beee22dbec4f7ff2506694a14e5a21395
bharat-reddy-male <bharatreddy1994@gmail.com>
2020-10-09 21:21:42 -0500

    Navbar differentiated between admin and user

--- Codes/isa_nwmsu/client/src/components/events/upcomingevents.js
+++ Codes/isa_nwmsu/client/src/components/events/upcomingevents.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import Navbar from "../layout/Navbar";
+import AdminNavbar from "../layout/AdminNavbar";
+import { renderCustomComponent } from 'react-chat-widget';
+
+let navbar = undefined;
+
+class UpcomingEvents extends Component{
+
+    componentWillMount(){
+        if (localStorage.getItem('jwtToken') != null) {
+            this.setState({ loggedIn: true });
+          }
+          if (localStorage.getItem('role') === null || localStorage.getItem('role') === '50') {
+            navbar = <Navbar />
+          }
+          if (localStorage.getItem('role') === '100') {
+            navbar = <AdminNavbar />
+          }
+    }
 
 
-
-const UpcomingEvents=()=>{
+render(){
     return(
+        <div>
+            {navbar}
         <div className="ui container">
             <br/>
             <button class="ui primary right floated button">Create Event</button>
             </div>
         
            
-        
+        </div>
 
     );
-
-};
+}
+}
 
 export default UpcomingEvents;
