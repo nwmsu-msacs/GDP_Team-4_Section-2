a45790ebc0c2e5687081165e737c6693b9589f22
bharat-reddy-male <bharatreddy1994@gmail.com>
2020-10-09 21:21:26 -0500

    Navbar differentiated between admin and user

--- Codes/isa_nwmsu/client/src/components/events/pastevents.js
+++ Codes/isa_nwmsu/client/src/components/events/pastevents.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {Component} from 'react';
 import holyimage from '../../assets/carousel_image1.jpg';
 import diwaliimage from '../../assets/carousel_image2.jpg';
 import independenceimage from '../../assets/carousel_image3.jpg';
 import Indiannightimage from '../../assets/carousel_image4.jpg';
 import Navbar from "../layout/Navbar";
+import AdminNavbar from "../layout/AdminNavbar";
 
+let navbar = undefined;
 
 const eventslist =
     [
@@ -60,17 +62,42 @@ const rendereventslist =
     }
     );
 
-const pasteventslist = () => {
+
+
+
+class PastEvents extends Component {
+
+
+  componentWillMount(){
+      console.log("------events------")
+    if (localStorage.getItem('jwtToken') != null) {
+      this.setState({ loggedIn: true });
+    }
+    if (localStorage.getItem('role') === null || localStorage.getItem('role') === '50') {
+      navbar = <Navbar />
+    }
+    if (localStorage.getItem('role') === '100') {
+      navbar = <AdminNavbar />
+    }
+  }
+
+  render() {
+
     return (
         <div>
-            {/* <Navbar /> */}
-            <br />
-            <div className="ui container" >
+        {navbar}
+        
+      <div>
+        
+        <div className="ui container" >
                 <h3 style={{ textAlign: 'center' }}>Past Events</h3><br />
                 {rendereventslist}
             </div>
-        </div>
+        
+      </div>
+      </div>
     );
-};
+  }
+}
 
-export default pasteventslist;
+export default PastEvents;
