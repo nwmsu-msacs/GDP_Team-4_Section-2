f5a41705fc7ac55aee3d86ce2c865c9cd254b379
bharat-reddy-male <bharatreddy1994@gmail.com>
2020-10-11 15:21:22 -0500

    minor code for accept and reject

--- Codes/isa_nwmsu/client/src/components/adminViews/AccommodationManagement.js
+++ Codes/isa_nwmsu/client/src/components/adminViews/AccommodationManagement.js
@@ -5,6 +5,20 @@ import Navbar from "../layout/Navbar";
 
 let navbar = undefined;
 
+function accept(data){
+
+  // e.preventDefault();
+  console.log("-----inside accept----")
+
+  const accommodationdata = data;
+
+  axios.post('http://localhost:5000/api/services/accommodationaccept', accommodationdata)
+          .then(res => console.log(res.data))
+          .then("/accommodationManagement")
+
+
+};
+
 const AccommodationMap = (accommodationList) => {
   console.log("-----accommodationList", accommodationList);
   
@@ -18,10 +32,11 @@ const AccommodationMap = (accommodationList) => {
         <div class="col-lg-4 col-md-6 mb-lg-0 mb-5" >
           
           <h5 class="font-weight-bold mt-4 mb-3">{data.firstName}&nbsp;{data.lastName}</h5>
+          <p class="text-uppercase blue-text"><strong>Email: {data.email}</strong></p>
           <p class="text-uppercase blue-text"><strong>Contact No: {data.contactNo}</strong></p>
           <p class="text-uppercase blue-text"><strong>919#: {data.non}</strong></p>
           <p class="text-uppercase blue-text"><strong>Days Required:  {data.daysRequired}</strong></p>
-          
+          <p class="text-uppercase blue-text"><strong>Status:  {data.status}</strong></p>
           <button
                   style={{
                     width: "150px",
@@ -31,6 +46,7 @@ const AccommodationMap = (accommodationList) => {
                   }}
                   type="submit"
                   className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                  onClick = {accept(data)}
                 >
                   Accept
                 </button>&nbsp;&nbsp;
