16a4581486a8b48e9f5882f9e5332712af33e1e1
bharat-reddy-male <bharatreddy1994@gmail.com>
2020-10-11 15:25:01 -0500

    Navbar Register component

---
+++ Codes/isa_nwmsu/client/src/components/layout/NavRegister.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+class NavRegister extends Component {
+
+
+    constructor(props) {
+        super(props)
+    }
+    render() {
+
+        return (
+
+            <ul>
+                <li>
+                    <a className='nav-link' href='/register'>
+                        Resister
+                    </a>
+                </li>
+            </ul>
+
+        );
+
+    };
+}
+
+export default NavRegister;
