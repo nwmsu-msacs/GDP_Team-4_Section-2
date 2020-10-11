de9968452323b781d2e50ad70797f2ea5b0ef1c8
bharat-reddy-male <bharatreddy1994@gmail.com>
2020-10-11 15:24:47 -0500

    Navbar logout component

---
+++ Codes/isa_nwmsu/client/src/components/layout/NavLogout.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { logoutUser } from "../../actions/authActions";
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
+
+class NavLogout extends Component {
+
+
+    constructor(props) {
+        super(props)
+    }
+
+    onLogoutClick = e => {
+        console.log("---nav logout---")
+        e.preventDefault();
+        this.props.logoutUser();
+        this.setState({loggedIn: false}); 
+        // localStorage.removeItem("jwtToken");
+        // localStorage.removeItem("email");
+        // localStorage.removeItem("role");
+        
+      };
+
+    render() {
+
+        return (
+
+            <ul>
+                <li>
+                    <a className='nav-link' href='/' onClick ={this.onLogoutClick}>
+                        Logout
+                    </a>
+                </li>
+            </ul>
+
+        );
+
+    };
+}
+
+NavLogout.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+  };
+  const mapStateToProps = state => ({
+    auth: state.auth
+  });
+  export default connect(
+    mapStateToProps,
+    { logoutUser }
+  )(NavLogout);
