edc2948ee0027d88040732f409b79edde2eae993
bharat-reddy-male <bharatreddy1994@gmail.com>
2020-10-13 03:55:35 -0500

    accept, reject requests functionality implemented

--- Codes/isa_nwmsu/client/src/components/adminViews/VolunteerManagement.js
+++ Codes/isa_nwmsu/client/src/components/adminViews/VolunteerManagement.js
@@ -4,23 +4,39 @@ import AdminNavbar from "../layout/AdminNavbar";
 import Navbar from "../layout/Navbar";
 
 let navbar =  undefined;
-const VolunteerMap = (volunteerList) => {
+
+function reject(data, self) {
+
+  axios.post('http://localhost:5000/api/services/volunteerreject', data)
+    .then(res => self.go())
+    .catch(err => console.log(err));
+};
+
+function accept(data, self) {
+
+  axios.post('http://localhost:5000/api/services/volunteeraccept', data)
+    .then(res => self.go())
+    .catch(err => console.log(err));
+};
+
+const VolunteerMap = (volunteerList, self) => {
   console.log("-----volunteerList", volunteerList);
   
   let res = volunteerList.map((data) => {
     return (
 
 
-      <div class="row">
+      <div class="row" key={data._id}>
 
         {/* <!-- Grid column --> */}
         <div class="col-lg-4 col-md-6 mb-lg-0 mb-5" >
           
           <h5 class="font-weight-bold mt-4 mb-3">{data.firstName}&nbsp;{data.lastName}</h5>
+          <p class="text-uppercase blue-text"><strong>Email: {data.email}</strong></p>
           <p class="text-uppercase blue-text"><strong>Contact No: {data.contactNo}</strong></p>
           <p class="text-uppercase blue-text"><strong>919# : {data.non}</strong></p>
           <p class="text-uppercase blue-text"><strong>car Type:  {data.carType}</strong></p>
-          
+          <p class="text-uppercase blue-text"><strong>Status:  {data.status}</strong></p>
           
 
               <button
@@ -32,6 +48,7 @@ const VolunteerMap = (volunteerList) => {
                   }}
                   type="submit"
                   className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                  onClick={() => {accept(data, self)}}
                 >
                   Accept
                 </button>&nbsp;&nbsp;
@@ -45,6 +62,7 @@ const VolunteerMap = (volunteerList) => {
                   }}
                   type="submit"
                   className="btn btn-large waves-effect waves-light hoverable red accent-3"
+                  onClick={() => {reject(data, self)}}
                 >
                   Reject
                 </button>      
@@ -63,11 +81,21 @@ const VolunteerMap = (volunteerList) => {
 }
 
 class VolunteerManagement extends Component {
+
+  constructor(props){
+    super(props)
+  }
+
   state = {
-    volunteerData: []
+    volunteerData: [],
+    self: null
   }
 
   componentDidMount() {
+
+    this.setState({
+      self:this.props.history
+    });
     axios
       .get("http://localhost:5000/api/admin/volunteerManagement")
       .then(res => {
@@ -102,7 +130,7 @@ class VolunteerManagement extends Component {
         <div>
         <br/>
         <h2>Volunteer Management</h2>
-        <p>{VolunteerMap(this.state.volunteerData)}</p>
+        <p>{VolunteerMap(this.state.volunteerData, this.state.self)}</p>
         </div>
       </div>
     );
