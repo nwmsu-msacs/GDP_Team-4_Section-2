2690fbb381644a6d7a25b84f105a01899725fb1d
Bhavya-123 <54381716+Bhavya-123@users.noreply.github.com>
2020-10-14 22:35:45 -0500

    Replies schema

---
+++ Codes/isa_nwmsu/models/Replies.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//Schema for replies
+
+const ReplySchema = new Schema({
+
+    forumId: {
+        type: String,
+        required:true,
+    },
+    replyContent:{
+        type:String,
+        required:true
+    },
+    replyBy:{
+        type:String,
+        required:true
+    },
+    replyTime:{
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports = Replies = mongoose.model("replies", ReplySchema);
