a4230d6ada8716bfc5759cf7c1fefba4b69a89bc
Bhavya-123 <54381716+Bhavya-123@users.noreply.github.com>
2020-10-14 22:36:53 -0500

    forum schema updated with new schema

--- Codes/isa_nwmsu/models/Forum.js
+++ Codes/isa_nwmsu/models/Forum.js
@@ -1,22 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-//Schema for replies
-
-// const ReplySchema = new Schema({
-//     replyContent:{
-//         type:String,
-//         required:true
-//     },
-//     replyBy:{
-//         type:String,
-//         required:true
-//     },
-//     replyTime:{
-//         type: Date,
-//         default: Date.now()
-//     }
-// });
 
 // Schema for single forum
 const ForumSchema = new Schema({
