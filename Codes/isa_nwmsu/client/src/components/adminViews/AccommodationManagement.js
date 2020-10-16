67bb778c7b25f6c07c9fb826921b2e2ef15fe684
bharat-reddy-male <bharatreddy1994@gmail.com>
2020-10-16 05:54:30 -0500

    enhanced UI

--- Codes/isa_nwmsu/client/src/components/adminViews/AccommodationManagement.js
+++ Codes/isa_nwmsu/client/src/components/adminViews/AccommodationManagement.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import React, { Component } from 'react';
 import AdminNavbar from "../layout/AdminNavbar";
 import Navbar from "../layout/Navbar";
-
+import {  Card } from 'react-bootstrap'
+import {Button} from 'semantic-ui-react'
 let navbar = undefined;
 
 function reject(data, self) {
@@ -30,7 +31,52 @@ const AccommodationMap = (accommodationList, self) => {
 
         {/* <!-- Grid column --> */}
 
-        <div class="col-lg-4 col-md-6 mb-lg-0 mb-5" >
+        {/* <Card.Group> */}
+        <Card style={{ width: '50rem'}}>
+          <Card.Body>
+            <Card.Title>{data.firstName}&nbsp;{data.lastName}</Card.Title>
+            <br/>
+            <Card.Subtitle className="mb-2 text-muted"><i class="material-icons" style={{ color: "grey", opacity: "90%" }}>phone</i>{data.contactNo}</Card.Subtitle>
+            <Card.Text>
+            <p class="text-uppercase blue-text"><strong>Email: {data.email}</strong></p>
+          <p class="text-uppercase blue-text"><strong>919#: {data.non}</strong></p>
+          <p class="text-uppercase blue-text"><strong>Days Required:  {data.daysRequired}</strong></p>
+          <p class="text-uppercase blue-text"><strong>Status:  {data.status}</strong></p>
+    </Card.Text>
+    <p class="h4 text-center mb-4">
+    <Button
+            // style={{
+            //   width: "150px",
+            //   borderRadius: "3px",
+            //   letterSpacing: "1.5px",
+            //   marginTop: "1rem"
+            // }}
+            type="submit"
+            className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+            onClick={() => { accept(data, self) }}
+          >
+            Accept
+                </Button>&nbsp;&nbsp;
+
+                <Button
+            // style={{
+            //   width: "150px",
+            //   borderRadius: "3px",
+            //   letterSpacing: "1.5px",
+            //   marginTop: "1rem"
+            // }}
+            type="submit"
+            className="btn btn-large waves-effect waves-light hoverable red accent-3"
+            onClick= {() => {reject(data, self)}}
+          >
+            Reject
+                </Button></p>
+          </Card.Body>
+        </Card>
+        {/* </Card.Group> */}
+        {/* <div class="col-lg-4 col-md-6 mb-lg-0 mb-5" >
+
+          
 
           <h5 class="font-weight-bold mt-4 mb-3">{data.firstName}&nbsp;{data.lastName}</h5>
           <p class="text-uppercase blue-text"><strong>Accommodation Id: {data._id}</strong></p>
@@ -67,7 +113,7 @@ const AccommodationMap = (accommodationList, self) => {
             Reject
                 </button>
           <hr />
-        </div>
+        </div> */}
 
 
       </div>
@@ -128,8 +174,10 @@ class AccommodationManagement extends Component {
         {navbar}
         <div>
           <br />
-          <h2>Accommodation Management</h2>
-          <p>{AccommodationMap(this.state.AccommodationData, this.state.self)}</p>
+          <h2 class="text-center" style={{fontFamily:"Arial"}}>Accommodation Management</h2>
+          <div class="container" style={{ columns: "3", width:"100%" }}>
+            <p>{AccommodationMap(this.state.AccommodationData, this.state.self)}</p>
+          </div>
         </div>
       </div>
     );
