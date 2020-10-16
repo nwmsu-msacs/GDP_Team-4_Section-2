c6cfc5087d124e9e8500e0ebddbd4d89f0ff63b0
bharat-reddy-male <bharatreddy1994@gmail.com>
2020-10-16 05:54:57 -0500

    enhanced UI

--- Codes/isa_nwmsu/client/src/components/adminViews/PickupManagement.js
+++ Codes/isa_nwmsu/client/src/components/adminViews/PickupManagement.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import React, { Component } from 'react';
 import AdminNavbar from "../layout/AdminNavbar";
 import Navbar from "../layout/Navbar";
-
+import {  Card } from 'react-bootstrap'
+import {Button} from 'semantic-ui-react'
 let navbar = undefined;
 
 function reject(data, self) {
@@ -28,8 +29,53 @@ const pickUpMap = (pickupList, self) => {
 
       <div class="row" key = {data._id}>
 
+<Card style={{ width: '50rem'}}>
+          <Card.Body>
+            <Card.Title>{data.name}</Card.Title>
+            <br/>
+            <Card.Subtitle className="mb-2 text-muted"><i class="material-icons" style={{ color: "grey", opacity: "90%" }}>phone</i>{data.cell}</Card.Subtitle>
+            <Card.Text>
+            <p class="text-uppercase blue-text"><strong>Pickup Date: {data.date.substring(0,10)}</strong></p>
+          <p class="text-uppercase blue-text"><strong>Pickup Time: {data.date.substring(11,19)}</strong></p>
+          <p class="text-uppercase blue-text"><strong>Email:  {data.email}</strong></p>
+          <p class="text-uppercase blue-text"><strong>From:  {data.from} &nbsp; &nbsp; To: {data.to}</strong></p>
+          <p class="text-uppercase blue-text"><strong>Airline:  {data.airline} &nbsp;&nbsp; FlightNo: {data.flightNo}</strong></p>
+          <p class="text-uppercase blue-text"><strong>Luggage:  {data.luggage}</strong></p>
+          <p class="text-uppercase blue-text"><strong>Status:  {data.status}</strong></p>
+    </Card.Text>
+    <p class="h4 text-center mb-4">
+    <Button
+            // style={{
+            //   width: "150px",
+            //   borderRadius: "3px",
+            //   letterSpacing: "1.5px",
+            //   marginTop: "1rem"
+            // }}
+            type="submit"
+            className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+            onClick={() => { accept(data, self) }}
+          >
+            Accept
+                </Button>&nbsp;&nbsp;
+
+                <Button
+            // style={{
+            //   width: "150px",
+            //   borderRadius: "3px",
+            //   letterSpacing: "1.5px",
+            //   marginTop: "1rem"
+            // }}
+            type="submit"
+            className="btn btn-large waves-effect waves-light hoverable red accent-3"
+            onClick= {() => {reject(data, self)}}
+          >
+            Reject
+                </Button></p>
+          </Card.Body>
+        </Card>
+
         {/* <!-- Grid column --> */}
-        <div class="col-lg-4 col-md-6 mb-lg-0 mb-5" >
+        {/* <div class="col-lg-4 col-md-6 mb-lg-0 mb-5" >
          
           <h5 class="font-weight-bold mt-4 mb-3">{data.name}</h5>
           <p class="text-uppercase blue-text"><strong>Pickup Date: {data.date.substring(0,10)}</strong></p>
@@ -68,7 +114,7 @@ const pickUpMap = (pickupList, self) => {
                   Reject
                 </button>   
                 <hr/>
-        </div>
+        </div> */}
         
 
       </div>
@@ -132,8 +178,10 @@ class PickupManagement extends Component {
         {navbar}
         <div>
         <br/>
-        <h2>Pickup Management</h2>
+        <h2 class="text-center" style={{fontFamily:"Arial"}}>Pickup Management</h2>
+          <div class="container" style={{ columns: "3", width:"100%" }}>
         <p>{pickUpMap(this.state.pickUpdata, this.state.self)}</p>
+        </div>
         </div>
       </div>
     );
