32ab4bc0f0b7353baebebdc0f36cf78d0baf4630
Bhavya-123 <54381716+Bhavya-123@users.noreply.github.com>
2020-10-16 06:05:33 -0500

    Implemented semantic UI

--- Codes/isa_nwmsu/client/src/components/adminViews/AccommodationManagement.js
+++ Codes/isa_nwmsu/client/src/components/adminViews/AccommodationManagement.js
@@ -45,12 +45,7 @@ const AccommodationMap = (accommodationList, self) => {
     </Card.Text>
     <p class="h4 text-center mb-4">
     <Button
-            // style={{
-            //   width: "150px",
-            //   borderRadius: "3px",
-            //   letterSpacing: "1.5px",
-            //   marginTop: "1rem"
-            // }}
+            
             type="submit"
             className="btn btn-large waves-effect waves-light hoverable blue accent-3"
             onClick={() => { accept(data, self) }}
@@ -59,12 +54,7 @@ const AccommodationMap = (accommodationList, self) => {
                 </Button>&nbsp;&nbsp;
 
                 <Button
-            // style={{
-            //   width: "150px",
-            //   borderRadius: "3px",
-            //   letterSpacing: "1.5px",
-            //   marginTop: "1rem"
-            // }}
+            
             type="submit"
             className="btn btn-large waves-effect waves-light hoverable red accent-3"
             onClick= {() => {reject(data, self)}}
@@ -74,47 +64,7 @@ const AccommodationMap = (accommodationList, self) => {
           </Card.Body>
         </Card>
         {/* </Card.Group> */}
-        {/* <div class="col-lg-4 col-md-6 mb-lg-0 mb-5" >
-
-          
-
-          <h5 class="font-weight-bold mt-4 mb-3">{data.firstName}&nbsp;{data.lastName}</h5>
-          <p class="text-uppercase blue-text"><strong>Accommodation Id: {data._id}</strong></p>
-          <p class="text-uppercase blue-text"><strong>Email: {data.email}</strong></p>
-          <p class="text-uppercase blue-text"><strong>Contact No: {data.contactNo}</strong></p>
-          <p class="text-uppercase blue-text"><strong>919#: {data.non}</strong></p>
-          <p class="text-uppercase blue-text"><strong>Days Required:  {data.daysRequired}</strong></p>
-          <p class="text-uppercase blue-text"><strong>Status:  {data.status}</strong></p>
-          <button
-            style={{
-              width: "150px",
-              borderRadius: "3px",
-              letterSpacing: "1.5px",
-              marginTop: "1rem"
-            }}
-            type="submit"
-            className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-            onClick={() => { accept(data, self) }}
-          >
-            Accept
-                </button>&nbsp;&nbsp;
-
-                <button
-            style={{
-              width: "150px",
-              borderRadius: "3px",
-              letterSpacing: "1.5px",
-              marginTop: "1rem"
-            }}
-            type="submit"
-            className="btn btn-large waves-effect waves-light hoverable red accent-3"
-            onClick= {() => {reject(data, self)}}
-          >
-            Reject
-                </button>
-          <hr />
-        </div> */}
-
+        
 
       </div>
 
