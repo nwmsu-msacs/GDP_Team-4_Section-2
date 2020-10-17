e559a32289a71bec4ddcdb10e37eac1b861127cf
bharat-reddy-male <bharatreddy1994@gmail.com>
2020-10-16 22:01:27 -0500

    routes for home page

--- Codes/isa_nwmsu/server.js
+++ Codes/isa_nwmsu/server.js
@@ -12,6 +12,7 @@ const services = require('./routes/api/services');
 const chat = require('./routes/api/chat');
 const admin = require('./routes/api/admin');
 const forum = require("./routes/api/forum");
+const home = require("./routes/api/home");
 const app = express();
 
 // use Helmet middleware to automatically set secure HTTP headers
@@ -63,6 +64,7 @@ app.use("/api/services", services);
 app.use("/api/chat", chat);
 app.use("/api/admin", admin);
 app.use("/api/forum",forum);
+app.use("/api/home",home);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
