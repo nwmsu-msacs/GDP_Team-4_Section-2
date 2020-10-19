f96226adacbf8834ca4c0f04a8302d0a3232d30d
Bhavya-123 <54381716+Bhavya-123@users.noreply.github.com>
2020-10-19 01:50:14 -0500

    handle delete for user and admin

--- Codes/isa_nwmsu/client/src/components/forum/Replies.js
+++ Codes/isa_nwmsu/client/src/components/forum/Replies.js
@@ -6,14 +6,51 @@ import { Button, Comment, Form, Header } from 'semantic-ui-react'
 import image from "./user.png"
 let navbar = undefined;
 
+function deleteReply(data, self){
+  axios.post('http://localhost:5000/api/forum/deleteSingleReply', data)
+    .then(res => self.go())
+    .catch(err => console.log(err));
+    window.location.reload(false);
 
+}
 
-const ReplyMap = (replyList, self) => {
+const ReplyMap = (replyList, self, role, user) => {
   console.log("-----replyList", replyList);
 
   let res = replyList.map((data) => {
     return (
+<div>
+    {role === true || user === data.userEmail ?
+      <div class="row" key={data._id}>
 
+        {/* <!-- Grid column --> */}
+      <Comment.Group>
+      
+    <Comment>
+      
+      <Comment.Avatar
+      src= {image}>
+      </Comment.Avatar>
+      <Comment.Content>
+        <Comment.Author as='a'>{data.replyBy}</Comment.Author>
+        <Comment.Metadata>
+          <div>{data.replyTime.substring(0,10)}&nbsp;at&nbsp;{data.replyTime.substring(11,19)} </div>
+        </Comment.Metadata>
+        <Comment.Text><p style={{fontSize:"1.5em", textAlign:"justify"}}>{data.replyContent}</p></Comment.Text>
+        <Comment.Metadata>
+          <Button color="red" style={{fontSize:"0.5rem"}}onClick= {() => { deleteReply(data, self) }}>Delete Reply</Button>
+          
+        </Comment.Metadata>
+      </Comment.Content>
+    </Comment>
+        
+      </Comment.Group>
+        
+        
+
+
+      </div>
+      :
 
       <div class="row" key={data._id}>
 
@@ -40,7 +77,8 @@ const ReplyMap = (replyList, self) => {
 
 
       </div>
-
+  }
+      </div>
     );
   });
 
@@ -56,12 +94,16 @@ class ReplyView extends Component {
   }
   state = {
     ReplyData: [],
+    role:null,
+    user:null,
     self: null,
   }
 
   componentDidMount() {
     this.setState({
-      self: this.props.history
+      self: this.props.history,
+      role:this.state.role,
+      user:this.state.user
     });
 
     axios
@@ -77,6 +119,25 @@ class ReplyView extends Component {
       );
   }
 
+  componentWillMount() {
+    
+    if (localStorage.getItem('jwtToken') != null) {
+      this.setState({ loggedIn: true });
+    }
+    
+    if (localStorage.getItem('role') === null || localStorage.getItem('role') === '50') {
+      this.setState({role: false})
+      navbar = <Navbar />
+    }
+    if (localStorage.getItem('role') === '100') {
+      this.setState({role:true})
+      navbar = <AdminNavbar />
+    }
+    if(localStorage.getItem('email')!= null){
+      this.setState({user: localStorage.getItem('email')})
+    }
+  }
+
 
 
   render() {
@@ -89,7 +150,7 @@ class ReplyView extends Component {
     </Header>
 
       
-          <p>{ReplyMap(this.state.ReplyData, this.state.self)}</p>
+          <p>{ReplyMap(this.state.ReplyData, this.state.self, this.state.role, this.state.user)}</p>
           </div>
     );
   }
