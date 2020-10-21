c76556f4acf3671b6a01bddb2c83c84f42acd172
bharat-reddy-male <bharatreddy1994@gmail.com>
2020-10-21 01:36:59 -0500

    chat functionality

--- Codes/isa_nwmsu/server.js
+++ Codes/isa_nwmsu/server.js
@@ -9,7 +9,6 @@ const passport = require("passport");
 const users = require("./routes/api/users");
 const events = require('./routes/api/events');
 const services = require('./routes/api/services');
-const chat = require('./routes/api/chat');
 const admin = require('./routes/api/admin');
 const forum = require("./routes/api/forum");
 const home = require("./routes/api/home");
@@ -35,23 +34,7 @@ mongoose
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
 
-  //cometChat 
 
-// const appID = require("./config/keys").appID;
-// const apiKey = require("./config/keys").apiKey;
-// const agentUID = require("./config/keys").agentUID;
-
-const appID = '240050f8a85710c';
-const apiKey = '18d853100a77e50aed9fb3d75b4940ef552a4f18';
-const agentUID = 'admin'
-
-
-
-const headers = {
-  'Content-Type': 'application/json',
-  appid: appID,
-  apikey: apiKey,
-};
 
 // Passport middleware
 app.use(passport.initialize());
@@ -61,7 +44,6 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/events", events);
 app.use("/api/services", services);
-app.use("/api/chat", chat);
 app.use("/api/admin", admin);
 app.use("/api/forum",forum);
 app.use("/api/home",home);
